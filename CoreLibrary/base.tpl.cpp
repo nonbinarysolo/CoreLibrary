5f1eb8d7c0a3fd2a15706ed2f50a761ade6870ac
Jeff Thompson <jeff@thefirst.org>
2020-03-12 19:13:48 +0100

    global: Tabs to spaces, indentation, remove trailing whitespace.
    Update pipe.tpl.cpp
    
    Update pipe.tpl.cpp
    
    Update pipe.tpl.cpp

--- CoreLibrary/base.tpl.cpp
+++ CoreLibrary/base.tpl.cpp
@@ -8,10 +8,10 @@
 //_/_/   http://cadia.ru.is
 //_/_/ Copyright(c)2012
 //_/_/
-//_/_/ This software was developed by the above copyright holder as part of 
-//_/_/ the HUMANOBS EU research project, in collaboration with the 
+//_/_/ This software was developed by the above copyright holder as part of
+//_/_/ the HUMANOBS EU research project, in collaboration with the
 //_/_/ following parties:
-//_/_/ 
+//_/_/
 //_/_/ Autonomous Systems Laboratory
 //_/_/   Technical University of Madrid, Spain
 //_/_/   http://www.aslab.org/
@@ -35,123 +35,123 @@
 //_/_/
 //_/_/ --- HUMANOBS Open-Source BSD License, with CADIA Clause v 1.0 ---
 //_/_/
-//_/_/ Redistribution and use in source and binary forms, with or without 
-//_/_/ modification, is permitted provided that the following conditions 
+//_/_/ Redistribution and use in source and binary forms, with or without
+//_/_/ modification, is permitted provided that the following conditions
 //_/_/ are met:
 //_/_/
-//_/_/ - Redistributions of source code must retain the above copyright 
-//_/_/ and collaboration notice, this list of conditions and the 
+//_/_/ - Redistributions of source code must retain the above copyright
+//_/_/ and collaboration notice, this list of conditions and the
 //_/_/ following disclaimer.
 //_/_/
-//_/_/ - Redistributions in binary form must reproduce the above copyright 
+//_/_/ - Redistributions in binary form must reproduce the above copyright
 //_/_/ notice, this list of conditions and the following
-//_/_/ disclaimer in the documentation and/or other materials provided 
+//_/_/ disclaimer in the documentation and/or other materials provided
 //_/_/ with the distribution.
 //_/_/
-//_/_/ - Neither the name of its copyright holders nor the names of its 
-//_/_/ contributors may be used to endorse or promote products 
+//_/_/ - Neither the name of its copyright holders nor the names of its
+//_/_/ contributors may be used to endorse or promote products
 //_/_/ derived from this software without specific prior written permission.
 //_/_/
-//_/_/ - CADIA Clause: The license granted in and to the software under this 
-//_/_/ agreement is a limited-use license. The software may not be used in 
-//_/_/ furtherance of: 
-//_/_/ (i) intentionally causing bodily injury or severe emotional distress 
-//_/_/ to any person; 
-//_/_/ (ii) invading the personal privacy or violating the human rights of 
-//_/_/ any person; or 
+//_/_/ - CADIA Clause: The license granted in and to the software under this
+//_/_/ agreement is a limited-use license. The software may not be used in
+//_/_/ furtherance of:
+//_/_/ (i) intentionally causing bodily injury or severe emotional distress
+//_/_/ to any person;
+//_/_/ (ii) invading the personal privacy or violating the human rights of
+//_/_/ any person; or
 //_/_/ (iii) committing or preparing for any act of war.
 //_/_/
 //_/_/ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//_/_/ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//_/_/ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
-//_/_/ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//_/_/ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//_/_/ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//_/_/ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-//_/_/ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
-//_/_/ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//_/_/ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//_/_/ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//_/_/ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//_/_/ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//_/_/ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//_/_/ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//_/_/ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//_/_/ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//_/_/ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//_/_/ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //_/_/ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //_/_/
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
-namespace	core{
+namespace core {
 
-	template<class	C>	inline	P<C>::P():object(NULL){
-	}
+template<class C> inline P<C>::P() : object(NULL) {
+}
 
-	template<class	C>	inline	P<C>::P(C	*o):object(o){
+template<class C> inline P<C>::P(C *o) : object(o) {
 
-		if(object)
-			object->incRef();
-	}
+  if (object)
+    object->incRef();
+}
 
-	template<class	C>	inline	P<C>::P(const P<C>	&p):object(p.object){
+template<class C> inline P<C>::P(const P<C> &p) : object(p.object) {
 
-		if(object)
-			object->incRef();
-	}
+  if (object)
+    object->incRef();
+}
 
-	template<class	C>	inline	P<C>::~P(){
+template<class C> inline P<C>::~P() {
 
-		if(object)
-			object->decRef();
-	}
+  if (object)
+    object->decRef();
+}
 
-	template<class	C>	inline	C	*P<C>::operator	->()	const{
+template<class C> inline C *P<C>::operator ->() const {
 
-		return	(C	*)object;
-	}
+  return (C *)object;
+}
 
-	template<class	C>	inline	bool	P<C>::operator	==(C	*c)	const{
+template<class C> inline bool P<C>::operator ==(C *c) const {
 
-		return	object==c;
-	}
+  return object == c;
+}
 
-	template<class	C>	inline	bool	P<C>::operator	!=(C	*c)	const{
+template<class C> inline bool P<C>::operator !=(C *c) const {
 
-		return	object!=c;
-	}
+  return object != c;
+}
 
-	template<class	C>	template<class	D>	inline	bool	P<C>::operator	==(P<D>	&p)	const{
+template<class C> template<class D> inline bool P<C>::operator ==(P<D> &p) const {
 
-		return	object==p.object;
-	}
+  return object == p.object;
+}
 
-	template<class	C>	template<class	D>	inline	bool	P<C>::operator	!=(P<D>	&p)	const{
+template<class C> template<class D> inline bool P<C>::operator !=(P<D> &p) const {
 
-		return	object!=p.object;
-	}
+  return object != p.object;
+}
 
-	template<class	C>	inline	bool	P<C>::operator	!()	const{
+template<class C> inline bool P<C>::operator !() const {
 
-		return	!object;
-	}
+  return !object;
+}
 
-	template<class	C>	inline	P<C>&	P<C>::operator	=(C	*c){
+template<class C> inline P<C>& P<C>::operator =(C *c) {
 
-		if(object==c)
-			return	*this;
-		if(object)
-			object->decRef();
-		if(object=c)
-			object->incRef();
+  if (object == c)
+    return *this;
+  if (object)
+    object->decRef();
+  if (object = c)
+    object->incRef();
 
-		return	*this;
-	}
+  return *this;
+}
 
-	template<class	C>	template<class	D>	inline	P<C>	&P<C>::operator	=(const P<D>	&p){
+template<class C> template<class D> inline P<C> &P<C>::operator =(const P<D> &p) {
 
-		return	this->operator	=((C	*)p.object);
-	}
+  return this->operator =((C *)p.object);
+}
 
-	template<class	C>	inline	P<C>	&P<C>::operator	=(const P<C>	&p){
+template<class C> inline P<C> &P<C>::operator =(const P<C> &p) {
 
-		return	this->operator	=((C	*)p.object);
-	}
+  return this->operator =((C *)p.object);
+}
 
-	////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
-	template<class	C>	inline	_ObjectAdapter<C>::_ObjectAdapter():_Object(),C(){
-	}
+template<class C> inline _ObjectAdapter<C>::_ObjectAdapter() : _Object(), C() {
+}
 }
