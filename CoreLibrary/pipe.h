0da3536a646afb3587c25846b9b5fda25894dbb1
Jeff Thompson <jeff@thefirst.org>
2020-04-12 14:59:57 +0200

    PipeNN (minor): Use triple-slash doc comments.

--- CoreLibrary/pipe.h
+++ CoreLibrary/pipe.h
@@ -150,13 +150,14 @@ public:
   ~PipeNN();
   void clear();
   void push(T &t);
-  /**
-   * Pop the head item.
-   * @param waitForItem (optional) If true and the pipe is empty, block until
-   * another thread adds an item with push(). If false and the pipe is empty,
-   * return NULL immediately. If omitted, then wait for an item.
-   * @return The popped item, or NULL if waitForItem is false and the pipe is empty.
-   */
+
+  /// <summary>
+  /// Pop the head item.
+  /// </summary>
+  /// <param name="waitForItem">(optional) If true and the pipe is empty, block until
+  /// another thread adds an item with push(). If false and the pipe is empty,
+  /// return NULL immediately.If omitted, then wait for an item.</param>
+  /// <returns>The popped item, or NULL if waitForItem is false and the pipe is empty.</returns>
   T pop(bool waitForItem = true);
 };
 #elif defined PIPE_2
